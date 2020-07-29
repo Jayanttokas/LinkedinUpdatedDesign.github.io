889e29056bbc3c5920f08209f03b91e8136d6b62
unknown <tokasjayant@gmail.com>
2020-07-29 21:17:24 +0530

    not completed yet

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const discoverMenu = (display) => {
     if (display === 1) {
         document.getElementById("discoverMenu").style.display="block"
+        document.getElementById("discoverMenu").classList.add("fadeIn");
         document.getElementById("main-container").style.opacity = 0.2;
     } else {
+        document.getElementById("discoverMenu").classList.remove("fadeIn");
         document.getElementById("discoverMenu").style.display="none"     
         document.getElementById("main-container").style.opacity = 1;
+
     } 
 
 }
@@ -12,9 +15,31 @@ const discoverMenu = (display) => {
 const search = (searchShow) => {
     if (searchShow === 1) {
         document.getElementById("searchContainer").style.display="block";
+        document.getElementById("searchContainer").classList.add("fadeIn");
+        document.getElementById("main-container").style.opacity = 0.2;
     } else if (searchShow === 0) {
         document.getElementById("searchContainer").style.display="none";
+        document.getElementById("searchContainer").classList.remove("fadeIn");
+        document.getElementById("main-container").style.opacity = 1;
     }
     
+    
+}
+
+const messageBox = () => {
+    
+    if ( document.getElementById("messages").style.display === "none" ) {
+        document.getElementById("messages").style.display = "block";
+        document.getElementById("messages").classList.add("fadeIn");
+       
+    } else {
+        document.getElementById("messages").style.display = "none";
+        document.getElementById("messages").classList.remove("fadeIn");
+
+    }  
+
+}
+
+const like = (e) => {
     
 }
