b0a3272800ea8643789a3457c41426e1a6541bc4
unknown <tokasjayant@gmail.com>
2020-07-29 16:29:00 +0530

    Not yet complete

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const discoverMenu = (display) => {
     if (display === 1) {
         document.getElementById("discoverMenu").style.display="block"
-        document.getElementById("2ndMenu").style.display = "none";
         document.getElementById("main-container").style.opacity = 0.2;
     } else {
-        document.getElementById("discoverMenu").style.display="none"
-        document.getElementById("2ndMenu").style.display = "block";
+        document.getElementById("discoverMenu").style.display="none"     
         document.getElementById("main-container").style.opacity = 1;
     } 
 
+}
+
+const search = (searchShow) => {
+    if (searchShow === 1) {
+        document.getElementById("searchContainer").style.display="block";
+    } else if (searchShow === 0) {
+        document.getElementById("searchContainer").style.display="none";
+    }
+    
+    
 }
